9ded7688736deb767bbabf46573f04c1649581f6
Seunghyeok Lee <seunghyeok.tech@gmail.com>
2023-11-07 21:18:01 +0900

    ObjectType

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
   imports: [
+    RestaurantsModule,
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
+      autoSchemaFile: true,
     }),
   ],
   controllers: [],
