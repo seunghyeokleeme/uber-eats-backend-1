7a84a80a339b7b51ad4a92b2bb308d5adb20f34a
Seunghyeok Lee <seunghyeok.tech@gmail.com>
2023-11-10 03:54:09 +0900

    TypeORM Setup & Introducing ConfigService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,31 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
-import { join } from 'path';
 import { RestaurantsModule } from './restaurants/restaurants.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    RestaurantsModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'iseunghyeog',
+      password: 'test',
+      database: 'uber-eats',
+      synchronize: true,
+      logging: true,
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: true,
     }),
+    RestaurantsModule,
   ],
   controllers: [],
   providers: [],
